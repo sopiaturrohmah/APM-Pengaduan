e2ed929b98e8387f11df684e8d97d859fa5c4ff0
sopiaturrohmah <sopiaturrohmah6@gmail.com>
2023-03-19 13:41:13 -1200

    Upload Awal

---
+++ app/Views/Layout/Footer.php
@@ -0,0 +1,10 @@
+<footer class="footer mt-auto py-3">
+<div class="container">
+<span class="text-muted">By Sopia Turrohmah</span>
+</div>
+</footer>
+<script src="/js/jquery-3.6.0.min.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+<script src="/js/App.js"></script>
+</body>
+</html>
